78079e4b21b7c2687da55aa90ec5373dc6c52f8a
Octavian Rotari <octavianrotari@gmail.com>
2016-04-14 13:11:54 +0200

    user can view main page

---
+++ spec/feature/user_visits_homepage_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+feature 'user visits homepage' do
+  scenario ' successfully' do
+    visit '/'
+    expect(page).to have_css 'h1', text: 'Contabilita'
+  end
+end
---
+++ spec/feature/users_sees_own_invoices_spec.rb
@@ -0,0 +1,8 @@
+#require 'rails_helper'
+
+#feature 'user sees own invoices' do
+  #scenario 'all the invoices' do
+    #Invoice.create(supplier: 'Bezzi', total:100, vat:21, date:'01/04/2015', plate:'de234ed', deadline:'30/04/2015', type_of_payment:'Bonifico')
+
+  #end
+#end
