88fac9ea08c19871de30f5bcc0b657274ed7a475
Octavian Rotari <octavianrotari@gmail.com>
2016-04-15 12:26:33 +0200

    user can add new invoice

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,15 +1,30 @@
 class InvoicesController < ApplicationController
+  before_filter :get_company, only: [:new, :create]
+
   def index
     @invoices = Invoice.all
   end
 
   def new
+    @invoice = Invoice.new
+  end
 
+  def create
+    invoice = @company.invoices.build(invoice_params)
+    if invoice.save
+      redirect_to company_path(@company.id)
+    else
+      redirect_to new_company_invoice
+    end
   end
 
   private
+  def invoice_params
+    params.require(:invoice).permit(:total, :date, :plate, :deadline, :type_of_payment)
+  end
 
-  def params_id
-    params[:company_id]
+  def get_company
+    @company = Company.find(params[:company_id])
   end
 end
+
--- spec/features/user_adds_new_invoice_spec.rb
+++ spec/features/user_adds_new_invoice_spec.rb
@@ -12,7 +12,19 @@ feature 'user' do
   end
 
   scenario 'Adds a new invoice' do
-    visit_invoice_page
-    click_link 'Aggiungi Fattura'
+    company = company('Bezzi')
+    visit '/'
+    click_link 'Aziende'
+    click_link 'Bezzi'
+    click_link 'Aggiungi fattura'
+    fill_in 'invoice[total]', with: 250
+    fill_in 'invoice[date]', with: "01/04/2015"
+    fill_in 'invoice[plate]', with: "EG324FR"
+    fill_in 'invoice[deadline]', with: "01/05/2015"
+    fill_in 'invoice[type_of_payment]', with: "bonifico"
+    click_button 'Aggiungi fattura'
+    expect(page).to have_css 'td', text: '250'
+    expect(page).to have_css 'td', text: 'EG324FR'
+    expect(page).to have_css 'td', text: 'bonifico'
   end
 end
