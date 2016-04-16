cba4a570206c440740da31e39c991c19f4fea706
Octavian Rotari <octavianrotari@gmail.com>
2016-04-16 12:32:13 +0200

    refactoring buildInvoice

--- app/models/build_invoice.rb
+++ app/models/build_invoice.rb
@@ -1,31 +1,51 @@
 class BuildInvoice
+
   def initialize invoice_params
     @invoice_params = invoice_params
   end
 
-  def vat
-    vat = @invoice_params[:taxable].to_f * @invoice_params[:vat].to_i/100
-    if(@invoice_params.has_key?("vat_1"))
-      vat_1 = @invoice_params[:taxable_1].to_f * @invoice_params[:vat_1].to_i/100
-      vat += vat_1
+  def total_vat
+    total_vat = @invoice_params[:taxable].to_f * @invoice_params[:vat].to_i/100
+    @invoice_params.delete(:vat)
+    total_vat = check_if_more_vat total_vat
+  end
+
+  def check_if_more_vat total_vat
+    i = 1
+    while i < 3
+      vat = 'vat_' + i.to_s
+      taxable = 'taxable_' + i.to_s
+      if(@invoice_params.has_key?(vat.to_sym))
+        total_vat += @invoice_params[taxable.to_sym].to_f * @invoice_params[vat.to_sym].to_i/100
+        @invoice_params.delete(vat.to_sym)
+      end
+      i += 1
     end
-    if(@invoice_params.has_key?("vat_2"))
-      vat_2 = @invoice_params[:taxable_2].to_f * @invoice_params[:vat_2].to_i/100
-      vat += vat_2
+    total_vat
+  end
+
+  def total_taxable
+    total_taxable = @invoice_params[:taxable].to_f
+    @invoice_params.delete(:taxable)
+    total_taxable = check_if_more_taxable total_taxable
+  end
+
+  def check_if_more_taxable total_taxable
+    i = 1
+    while i < 3
+      taxable = 'taxable_' + i.to_s
+      if(@invoice_params.has_key?(taxable.to_sym))
+        total_taxable += @invoice_params[taxable.to_sym].to_f
+        @invoice_params.delete(taxable.to_sym)
+      end
+      i += 1
     end
-    @invoice_params[:vat] = vat
-    vat
+    total_taxable
   end
 
   def total
-    total = vat + @invoice_params[:taxable].to_f
-    if(@invoice_params.has_key?("taxable_1"))
-      total += @invoice_params[:taxable_1].to_f
-    end
-    if(@invoice_params.has_key?("taxable_2"))
-      total += @invoice_params[:taxable_1].to_f
-    end
-    @invoice_params = @invoice_params.merge(total: total)
+    total = total_taxable + total_vat
+    @invoice_params = @invoice_params.merge(total: total, vat: total_vat)
     @invoice_params
   end
 end
