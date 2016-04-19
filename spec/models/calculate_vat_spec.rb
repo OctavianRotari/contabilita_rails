ef4300d51abd3e58b391c20b4a5ea682f9e74b87
Octavian Rotari <octavianrotari@gmail.com>
2016-04-19 10:34:34 +0200

    testing model vat taxable

--- spec/models/calculate_vat_spec.rb
+++ spec/models/calculate_vat_spec.rb
@@ -1,33 +1,33 @@
-#require 'spec_helper'
+require 'spec_helper'
 
-#describe CalculateVat do
-  #invoice_params = {taxable_1: "200",
-                    #vat_1: "4"}
-  #invoice_params_1 = {taxable_1: "200",
-                      #taxable_2: "1000",
-                      #vat_1: "4",
-                      #vat_2: "10"}
-  #invoice_params_2 = {taxable_1: "1000",
-                      #taxable_2: "100",
-                      #taxable_3: "2000",
-                      #vat_1: "10",
-                      #vat_2: "10",
-                      #vat_3: "10"}
+describe BuildInvoice do
+  invoice_params = {taxable_1: "200",
+                    vat_1: "4"}
+  invoice_params_1 = {taxable_1: "200",
+                      taxable_2: "1000",
+                      vat_1: "4",
+                      vat_2: "10"}
+  invoice_params_2 = {taxable_1: "1000",
+                      taxable_2: "100",
+                      taxable_3: "2000",
+                      vat_1: "10",
+                      vat_2: "10",
+                      vat_3: "10"}
 
-  #describe '#vat' do
-    #it 'should calculate vat' do
-      #vat = CalculateVat.new(invoice_params)
-      #expect(vat.total_vat).to equal(8.0)
-    #end
+  describe '#vat' do
+    it 'should calculate vat' do
+      vat = BuildInvoice.new(invoice_params)
+      expect(vat.total_vat(invoice_params)).to equal(8.0)
+    end
 
-    #it 'should calculate sum of two vat' do
-      #vat = CalculateVat.new(invoice_params_1)
-      #expect(vat.total_vat).to equal(108.0)
-    #end
+    it 'should calculate sum of two vat' do
+      vat = BuildInvoice.new(invoice_params_1)
+      expect(vat.total_vat(invoice_params_1)).to equal(108.0)
+    end
 
-    #it 'should calculate sum of three vat' do
-      #vat = CalculateVat.new(invoice_params_2)
-      #expect(vat.total_vat).to equal(310.0)
-    #end
-  #end
-#end
+    it 'should calculate sum of three vat' do
+      vat = BuildInvoice.new(invoice_params_2)
+      expect(vat.total_vat(invoice_params_2)).to equal(310.0)
+    end
+  end
+end
