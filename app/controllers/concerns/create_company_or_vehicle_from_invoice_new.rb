2e95fba04e8baf44b8d5afef3589575cc2272700
Octavian Rotari <octavianrotari@gmail.com>
2016-08-31 14:14:45 +0200

    user see errors in pop up forms if not saved

--- app/controllers/concerns/create_company_or_vehicle_from_invoice_new.rb
+++ app/controllers/concerns/create_company_or_vehicle_from_invoice_new.rb
@@ -11,25 +11,25 @@ module CreateCompanyOrVehicleFromInvoiceNew
   private
 
   def create_company
-    company = Company.new(company_params)
-    if company.save
+    @company = Company.new(company_params)
+    if @company.save
       flash[:notice] = 'Azienda inserita'
       redirect_to new_invoice_path
     else
-      redirect_to new_invoice_path
       flash[:notice] = 'Azienda non inserita'
+      render "new"
     end
   end
 
 
   def create_vehicle
-    vehicle = Vehicle.new(vehicle_params)
-    if vehicle.save
+    @vehicle = Vehicle.new(vehicle_params)
+    if @vehicle.save
       flash[:notice] = 'Veicolo inserito'
       redirect_to new_invoice_path
     else
       flash[:notice] = 'Veicolo non inserito'
-      redirect_to new_invoice_path
+      render "new"
     end
   end
 
