44f9b1df57c19dd16c9b19efb8b50c44e81d12c0
Octavian Rotari <octavianrotari@gmail.com>
2016-10-18 16:44:48 +0200

    precompiled for heroku

---
+++ public/assets/scripts-a72710e1e6aaf533688dfdfbaee94ca9bd1cf4e9bfd69da6ff8f8d3ae0df5ec0.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	//for stiky navigation
+	$('.js--main-navigation').waypoint(function(direction){
+		if(direction == "down"){
+			$('nav').addClass('sticky');
+		}
+		else {
+			$('nav').removeClass('sticky');
+		}
+	});
+
+	$(window).on('resize', function mobileNavMenu() {
+		var viewportWidth = $(window).width();
+		if (  viewportWidth < 767 ) {
+			$(".main-nav").removeClass("span-1-of-2").addClass("span-2-of-2");
+		}
+		if (  viewportWidth > 767 ){
+			$(".main-nav").removeClass("span-2-of-2").addClass("span-1-of-2");
+		}
+	});
+});
