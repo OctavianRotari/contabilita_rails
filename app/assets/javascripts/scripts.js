c12f75a8f594131523cbfe45131a6839785f0eaf
Octavian Rotari <octavianrotari@gmail.com>
2016-10-18 15:55:42 +0200

    media queries 468

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -8,4 +8,11 @@ $(document).ready(function(){
 			$('nav').removeClass('sticky');
 		}
 	});
+
+	$(window).ready(function() {
+		var viewportWidth = $(window).width();
+		if (viewportWidth < 767) {
+			$(".main-nav").removeClass("span-1-of-2").addClass("span-2-of-2");
+		}
+	});
 });
