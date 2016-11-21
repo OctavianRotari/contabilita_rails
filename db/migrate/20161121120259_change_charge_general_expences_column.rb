e057df35e129f678134f8366d6e0f82ff98bd734
Octavian Rotari <octavianrotari@gmail.com>
2016-11-21 13:05:52 +0100

    fixed bug migration for heroku

--- db/migrate/20161121120259_change_charge_general_expences_column.rb
+++ db/migrate/20161121120259_change_charge_general_expences_column.rb
@@ -1,5 +1,5 @@
 class ChangeChargeGeneralExpencesColumn < ActiveRecord::Migration
   def change
-    rename_column :vehicles, :charge_general_expences, :charge_general_expeses
+    rename_column :vehicles, :charge_general_expences, :charge_general_expenses
   end
 end
