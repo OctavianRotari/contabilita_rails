a523bb01c88bb1a5eeaa92a1d390596834ebc8e8
Octavian Rotari <octavianrotari@gmail.com>
2016-11-21 13:09:39 +0100

    issues renaming columns

--- db/migrate/20161121120259_change_charge_general_expences_column.rb
+++ db/migrate/20161121120259_change_charge_general_expences_column.rb
@@ -1,5 +1,5 @@
 class ChangeChargeGeneralExpencesColumn < ActiveRecord::Migration
   def change
-    rename_column :vehicles, :charge_general_expences, :charge_general_expenses
+    rename_column :vehicles, :charge_general_expences, :charge_general_expeses
   end
 end
