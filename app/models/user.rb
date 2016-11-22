544f561524b372e26bb650d817481683e6fc122f
Octavian Rotari <octavianrotari@gmail.com>
2016-11-22 13:50:47 +0100

    syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :companie, dependent: :destroys
+  has_many :companies, dependent: :destroy
   has_many :invoices, dependent: :destroy
   has_many :payments, dependent: :destroy
   has_many :vehicles, dependent: :destroy
